edf9e768b5473a20d00384920fd9c02884c7e6e3
programminglounge <104110944+programminglounge@users.noreply.github.com>
2022-04-30 12:00:48 -0700

    Create avoidflood.cpp

---
+++ avoidflood.cpp
@@ -0,0 +1,76 @@
+// I solved this problem using a greedy approach
+//
+vector<int> avoidFlood(vector<int> & rains) {
+  int max = 0;
+  for (int i=0; i<rains.size(); i++)
+  {
+    if (rains[i] > max)
+    {
+      max = rains[i];
+    }
+  }
+  vector<int> floods(max); // define a flood for each lake
+  vector <int> lakefull; // determine which lake is full and add that to the vector
+  vector <int> result; // this is the output so far. This array has an equal length to the rains array
+  vector<int> result2 = {}; // if the output is not possible, we return an empty result
+  for (int i = 0; i<rains.size(); i++)
+  {
+    if (rains[i] != 0)
+    {
+      if (floods[rains[i]-1]==1)
+      {
+        return result2;
+      }
+      else if (floods[rains[i]-1] == 0)
+      {
+        floods[rains[i]-1]++;
+      }
+      lakefull.push_back(rains[i]);
+      result.push_back(-1);
+    }
+    else
+    {
+      //we will use a greedy algorithm where it will look at each lake which is 
+      //full and determine which one will show up faster in the sequence line and dry up that lake
+      int positions = rains.size();
+      int value = 0;
+      for (int j = 0; j<lakefull.size(); j++)
+      {
+        for (int k = i+1; k<rains.size(); k++) 
+        {
+          if (rains[k] == lakefull[j] && k< positions) 
+          {
+            positions = k;
+            value = j;
+            break;
+          }
+        }
+      }
+      if (positions != rains.size())
+      {
+        result.push_back(lakefull[value]);
+        floods[lakefull[value]-1]--;
+        lakefull.erase(lakefull.begin()+value);
+      }
+      else if (lakefull.size() != 0)
+      {
+        result.push_back(lakefull[value]);
+        floods[lakefull[value]-1]--;
+        lakefull.erase(lakefull.begin()+value);
+      }
+    }
+  }
+  return result;
+}
+
+int main()
+{
+  vector<int> rains = {1, 2, 0, 1, 2};
+  vector <int> rains2 = avoidFlood(rains);
+  for (int i = 0; i<rains2.size(); i++)
+  {
+    cout << rains2[i] << " ";
+  }
+  cout << endl;
+  return 0;
+}
