f84342227f387015f016b8c535490138caa8e73f
programminglounge <104110944+programminglounge@users.noreply.github.com>
2022-06-03 11:24:14 -0700

    Create MaximumProduct.cpp

---
+++ MaximumProduct.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+int maximumProduct (vector <int> & nums, int k)
+{
+  priority_queue<int, vector<int>, greater<int>> pq;
+  for (int i = 0; i<nums.size(); i++)
+  {
+    pq.push(nums[i]);
+  }
+  if (nums.size() != 0)
+  {
+    for (int i = 0; i<k; i++)
+    {
+      int s = pq.top();
+      pq.pop();
+      pq.push(s+1);
+    }
+    int sum = 1;
+    for (int i = 0; i<nums.size(); i++)
+    {
+      int s = pq.top();
+      pq.pop();
+      sum = sum * s;
+    }
+    return sum;
+  }
+  return 0;
+}
